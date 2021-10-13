7ec4ad4ff9453a23eab8c8f858fcbfadb2403062
tgodfrey0 <toby.godfrey2003@gmail.com>
2021-10-13 13:33:34 +0100

    Added main

---
+++ javadoc/member-search-index.js
@@ -0,0 +1,1 @@
+memberSearchIndex = [{"p":"com.scraper","c":"Main","l":"Main()","u":"%3Cinit%3E()"},{"p":"com.scraper","c":"Main","l":"main(String[])","u":"main(java.lang.String[])"}];updateSearchResults();
---
+++ src/com/scraper/Main.java
@@ -0,0 +1,23 @@
+/**
+ * <p>The package com.scraper contains all the files for the project</p>
+ *
+ * @author Toby Godfrey
+ */
+package com.scraper;
+
+import java.util.Scanner;
+
+/**
+ * <p>Main is the class which calls all the functions for the application to run, and it contains the *
+ * main method.</p>
+ */
+public class Main {
+
+  public static void main(String[] args) {
+    String username;
+    Scanner input = new Scanner(System.in);
+    System.out.print("Please enter the username: ");
+    username = input.nextLine();
+    
+  }
+}
